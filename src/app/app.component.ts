b72ffb46470d77490fbd2a80dc1129ea774411e2
sneha-kolli <kollisnehagowri@gmail.com>
2025-07-20 15:16:37 +0530

    f/home second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_app-main-login';
+  title = 'angular_app-feature-home';
 }
